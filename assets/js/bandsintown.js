d88d0de6cf948e959819e885ce0f2b048622d88d
reno-xjb <23520255+reno-xjb@users.noreply.github.com>
2026-06-02 23:17:31 +0200

    fix bandsintown

--- assets/js/bandsintown.js
+++ assets/js/bandsintown.js
@@ -18,7 +18,7 @@ const artistsAndAppIds = {
 		appId: "04aa449760082e70bc18eb1e6d1eae9d",
 	},
 	cetaces: {
-		name: `C<span class="acute">'</span>etac<span class="acute">'</span>es`,
+		name: "Cetaces",
 		link: "/cetaces",
 		artistId: "id_15648884",
 		appId: "60cd548be8d0ed105cec488911d03f33",
